acdf5da04412357a0f350e9a621dd3058b565a36
Gabriel B <gabriel.bfnds10@gmail.com>
2024-08-30 14:30:33 -0300

    comit 3

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ const myObserver = new IntersectionObserver((entries) => {
 const elements = document.querySelectorAll('.hidden')
 
 elements.forEach((element) => myObserver.observe(element))
+
+
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('#menu')
+    menu-h.classList.toggle('rolagem', window.scrollY >0)
+})
